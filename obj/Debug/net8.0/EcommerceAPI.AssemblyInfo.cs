29867c56e8b90dad51f779dbdb88a6284ec6766e
glayberson <glaybersonsilva@outlook.com>
2025-07-07 22:10:13 -0300

    Preparando merge

--- obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b0c0525908169548825b10dd8a4c04685f1b2ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa4a65533523fa6181cea86f66fd5cee47fafd8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
